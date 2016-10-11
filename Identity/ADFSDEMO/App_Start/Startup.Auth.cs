7a1d38ebaef7c78c9f2234aea8fbbc80ff234d79
Victor Chin <victor@objectmage.com>
2016-10-11 23:33:53 +0100

    Add ADFSDEMO using adfs.marseille.nyc

---
+++ Identity/ADFSDEMO/App_Start/Startup.Auth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.WsFederation;
+using Owin;
+using System.IdentityModel.Tokens;
+
+namespace ADFSDEMO
+{
+    public partial class Startup
+    {
+        private static string realm = ConfigurationManager.AppSettings["ida:Wtrealm"];
+        private static string adfsMetadata = ConfigurationManager.AppSettings["ida:ADFSMetadata"];
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseWsFederationAuthentication(
+                new WsFederationAuthenticationOptions
+                {
+                    TokenValidationParameters =
+                    new TokenValidationParameters { AudienceValidator =
+                    (aud, b, c) => true
+                    },
+                    Wtrealm = realm,
+                    MetadataAddress = adfsMetadata
+                });
+        }
+    }
+}
---
+++ Identity/ADFSDEMO/Controllers/ClaimsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ADFSDEMO.Controllers
+{
+    [Authorize]
+    public class ClaimsController : Controller
+    {
+        // GET: Claims
+        public ActionResult Index()
+        {
+            var ident = User.Identity as ClaimsIdentity;
+            return View(ident.Claims);
+        }
+    }
+}
